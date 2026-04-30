b39510a7466a6b7f1bac6848ed9d49ba9201923b
dannycastroaudio <dannycastroaudio@gmail.com>
2026-04-30 03:56:08 -0400

    updated a few things in code.
    i mixed up the thunder_near and thunder_far values. dumb.

--- CsoundUnity_MTEC340_DC/Assets/Scripts/TriggerCsound.cs
+++ CsoundUnity_MTEC340_DC/Assets/Scripts/TriggerCsound.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class TriggerCsound : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public CsoundUnity CsoundSource;
     
     
@@ -14,21 +13,22 @@ public class TriggerCsound : MonoBehaviour
     
     public void OnTriggerEnter(Collider other)
     {
+        //play wind. its a loop, wont stop until you stop it.
         if (other.gameObject.CompareTag("Wind"))
         {
             CsoundSource.SetChannel("Wind", 1f);
         }
-        
+        //play thunder. must be restarted after each play.
         else if (other.gameObject.CompareTag("Thunder"))
         {
             CsoundSource.SetChannel("Thunder", 1f);
         }
-        
+        //play rain. same as the wind, it will keep playing until you stop it
         else if (other.gameObject.CompareTag("Rain"))
         {
             CsoundSource.SetChannel("Rain", 1f);
         }
-        
+        //actions to stop/restart (in case of the thunder) the instruments
         else if (other.gameObject.CompareTag("StopWind"))
         {
             CsoundSource.SetChannel("Wind", 0f);
@@ -41,6 +41,9 @@ public class TriggerCsound : MonoBehaviour
         {
             CsoundSource.SetChannel("Rain", 0f);
         }
+        
+        //Parameter Controls!
+        
         //wind intensity. controls layers + levels of the wind instrument
         else if (other.gameObject.name == "wind_soft")
         {
@@ -59,7 +62,7 @@ public class TriggerCsound : MonoBehaviour
         }
         
         //thunder distance
-        else if (other.gameObject.name == "thunder_far")
+        else if (other.gameObject.name == "thunder_near")
         {
             CsoundSource.SetChannel("ThunderDist", 0.10f);
             CsoundSource.SetChannel("ThunderLev", 0.30f);
@@ -69,7 +72,7 @@ public class TriggerCsound : MonoBehaviour
             CsoundSource.SetChannel("ThunderDist", 0.50f);
             CsoundSource.SetChannel("ThunderLev", 0.50f);
         }
-        else if (other.gameObject.name == "thunder_near")
+        else if (other.gameObject.name == "thunder_far")
         {
             CsoundSource.SetChannel("ThunderDist", 1f);
             CsoundSource.SetChannel("ThunderLev", 1f);
